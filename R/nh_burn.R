2e6e74d89d82608488b2643af46fec330af31620
David Bucklin <david.bucklin@dcr.virginia.gov>
2018-03-20 15:22:57 -0400

    nh_burn reclassification set to >= orig.thresh

--- R/nh_burn.R
+++ R/nh_burn.R
@@ -5,21 +5,25 @@
 #' Takes spatial features, a continuous raster (values between 0 and 1), and 
 #' returns a classified binary (0/1) raster. Areas greater than \code{orig.thresh} are
 #' set to 1, all others 0. Areas intersecting \code{spf} features
-#' are assigned a value of 1 in the returned classified raster. 
+#' are assigned a value of 1 in the returned classified raster. See details
+#' for default calculation of \code{orig.thresh} and usage of \code{buffer}.
 #' 
-#' If \code{buffer} is not 0, areas within that buffer around \code{spf} that are greater 
-#' than the minimum cell value (min.cell) within \code{spf} are set to 1 
-#' in the classified raster.
+#' A minimum cell value (min.cell) across all \code{spf} is calculated.
+#' If \code{orig.thresh} is NULL, the min.cell value will be used as a threshold
+#' for the full raster.
 #' 
-#' If \code{orig.thresh} is NULL, the min.cell value will be used as a global threshold.
-#' When a given \code{orig.thresh} is less than the min.cell value, the result
-#' will be the same as a thresholded raster using \code{orig.thresh}.
+#' If \code{buffer} is greater than 0 \code{orig.thresh} is not NULL, \code{orig.thresh}
+#' is used as the threshold for the full raster. Additionally,
+#' cells within \code{buffer} distance of \code{spf} which are greater than min.cell
+#' are also set to 1. As a result, if a given \code{orig.thresh} is lower 
+#' than the calculated min.cell value, \code{buffer}
+#' distance will have no impact on the output.
 
 #' @param spf input spatial features (sp or sf spatial object)
 #' @param rast input raster model output (values between 0 and 1)
 #' @param orig.thresh numeric between 0 and 1; threshold value to apply to raster
 #' @param buffer numeric; spatial buffer around spf to include in burn-in
-#' @param ... additional paramaters to \code{raster::writeRaster}
+#' @param ... additional parameters to \code{raster::writeRaster}
 #' 
 #' @return RasterLayer
 #' 
@@ -64,8 +68,8 @@ nh_burn <- function(spf, rast, orig.thresh = NULL, buffer = 0, ...) {
     # case when buffer is 0 and threshold is given - simple burn in
     r2[r2 != 1] <- NA
     rout <- max(rast, extend(r2,rast), na.rm = T)
-    rcl <- data.frame(from = c(-1, orig.thresh), to = c(orig.thresh, 1.1), becomes = c(0,1))
-    rout <- reclassify(rout, rcl, ...)
+    rout[rout >= orig.thresh] <- 1
+    rout[rout != 1] <- 0
     return(rout)
   } else {
     # other cases, calculated the minimum value within features
@@ -79,9 +83,9 @@ nh_burn <- function(spf, rast, orig.thresh = NULL, buffer = 0, ...) {
     message("Original threshold is lower than minimum feature value. Returning classified raster...")
     # return thresholded rast, no changes needed
     # all areas covered by mval are already covered by orig.thresh
-    rcl <- data.frame(from = c(-1, orig.thresh), to = c(orig.thresh, 1.1), becomes = c(0,1))
-    rout <- reclassify(rast, rcl, ...)
-    return(rout)
+    rast[rast >= orig.thresh] <- 1
+    rast[rast != 1] <- 0
+    return(rast)
   }
   
   # update within buffer
@@ -91,8 +95,8 @@ nh_burn <- function(spf, rast, orig.thresh = NULL, buffer = 0, ...) {
     r3 <- rasterize(as(spfb, "Spatial"), r2, field = 1, background = NA)
     # find areas in buffer area above mval, set to 1
     r4 <- r2 + r3
-    r4[r4 < (mval+1)] <- NA
-    r4[!is.na(r4)] <- 1
+    r4[r4 >= (mval+1)] <- 1
+    r4[r4 != 1] <- NA
     r2 <- max(r2,r4, na.rm = T)
   }
   
@@ -101,7 +105,8 @@ nh_burn <- function(spf, rast, orig.thresh = NULL, buffer = 0, ...) {
   
   message("Reclassifying raster...")
   # reclassify
-  rcl <- data.frame(from = c(-1, orig.thresh), to = c(orig.thresh, 1.1), becomes = c(0,1)) 
-  rout <- reclassify(rout, rcl, ...)
+  rout[rout >= orig.thresh] <- 1
+  rout[rout != 1] <- 0
   return(rout)
+  
 }
