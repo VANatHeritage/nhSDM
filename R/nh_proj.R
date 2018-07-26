0a2a4f618a7cdf47e59164a6b2a1ceffbb9f90fe
David Bucklin <david.bucklin@dcr.virginia.gov>
2018-07-26 14:48:03 -0400

    update nh_proj

--- R/nh_proj.R
+++ R/nh_proj.R
@@ -1,33 +1,37 @@
-# nh_newproj
+# nh_proj
 
 #' Initiate new SDM project
 #' 
-#' Creates directory structure (if it doesn't exist) for SDMs and 
-#' new SDM project SQLite database. Folder structure is initiated in current
-#' working directory, unless \code{folder} is specified.
-#' 
-#' More details
+#' Creates directory structure for a new SDM project and 
+#' new SDM project SQLite database. New project folder is 
+#' created in working directory, unless \code{folder} is specified.
 #' 
 #' @param proj.name The name of the project. Appended to a new project database file.
 #' @param folder The SDM folder in which to initiate the project. Defaults to `getwd()`.
 #' 
-#' @return TRUE on succesful project creation
+#' @return nothing
 #'
 #' @author David Bucklin
 #'
 #' @import RSQLite
-#'
+#' 
+#' @export
 #'
 #' @examples
 #' \dontrun{
-#' setwd("D:/")
-#' nh_newproj("invert")
+#' setwd("D:/testing_SDM")
+#' nh_proj("dum")
 #' }
 
-nh_newproj <- function(proj.name, folder = "nhSDM") {
+nh_proj <- function(proj.name, folder = ".") {
   
-  s <- folder
-  if (!dir.exists(s)) dir.create(s)
+  s <- paste0(folder , "/", proj.name)
+  if (!dir.exists(s)) {
+    dir.create(s)
+  } else {
+    stop("Project folder at that location ('",s,"') already exists, choose a different name.")
+  }
+  
   # create folders
   dbfile <- paste0(s, "/databases/", "sdm_tracking_", proj.name, ".sqlite")
   if (file.exists(dbfile)) stop("Database file '", dbfile, "' already exists. Choose a different name or use nh_updateproj() to update database.")
@@ -36,7 +40,8 @@ nh_newproj <- function(proj.name, folder = "nhSDM") {
                "other_spatial/raster", "other_spatial/feature",
                "species/_template_copyme/inputs/background", "species/_template_copyme/inputs/presence",
                "species/_template_copyme/inputs/scripts","species/_template_copyme/inputs/model_input",
-               "species/_template_copyme/outputs")
+               "species/_template_copyme/outputs/rdata", "species/_template_copyme/outputs/metadata", 
+               "species/_template_copyme/outputs/model_predictions")
   
   for (i in folders) {
     if (!dir.exists(paste0(s, "/" , i))) dir.create(paste0(s, "/" , i), recursive = T)
@@ -53,5 +58,5 @@ nh_newproj <- function(proj.name, folder = "nhSDM") {
       dbCommit(conn)
       dbDisconnect(conn)
     })
-  message("New project database '", dbfile , "' created.")
+  message("New project and database '", dbfile , "' created.")
 }
